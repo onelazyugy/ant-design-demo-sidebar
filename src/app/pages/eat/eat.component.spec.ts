c25727250016b7c06193ca6c3741594513223714
Viet <viet_le@homedepot.com>
2020-04-29 16:48:00 -0400

    learing rxjs

---
+++ src/app/pages/eat/eat.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EatComponent } from './eat.component';
+
+describe('EatComponent', () => {
+  let component: EatComponent;
+  let fixture: ComponentFixture<EatComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ EatComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EatComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/eat/eat.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EatComponent } from './eat.component';
+
+
+
+@NgModule({
+  declarations: [EatComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class EatModule { }
