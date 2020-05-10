80c0f2ea6a42837b562231ad56520fd6ac4480c3
Viet <viet@Viets-MBP.attlocal.net>
2020-05-10 01:06:27 -0400

    wrap up checkout page

---
+++ src/app/model/order-summary.model.ts
@@ -0,0 +1,14 @@
+export class OrderSummary {
+    totalSelectedMeatTopping: number;
+    totalCostForSelectedMeatTopping: number;
+    totalSelectedVeggieTopping: number;
+    totalCostForSelectedVeggieTopping: number;
+    totalSelectedCheeseTopping: number;
+    totalCostForSelectedCheeseTopping: number;
+    pizzaBasePrice: number;
+    pizzaSize: string;
+    subtotal: number;
+    taxPercent: string;
+    totalTax: number;
+    totalDue: number;
+}
--- src/app/pages/pizza/checkout/checkout.component.ts
+++ src/app/pages/pizza/checkout/checkout.component.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../../../store/app.reducer';
+import { Subscription } from 'rxjs';
+import { OrderSummary } from 'src/app/model/order-summary.model';
+import { PizzaSize } from 'src/app/model/pizza.model';
+import _ from 'lodash';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
-export class CheckoutComponent implements OnInit {
-
+export class CheckoutComponent implements OnInit, OnDestroy {
+  toppingSubscription: Subscription;
   validateForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  orderSummary: OrderSummary;
+
+  totalSelectedCheeses: number;
+  totalSelectedMeats: number;
+  totalSelectedVeggies: number;
+  
+
+  constructor(private fb: FormBuilder, private store: Store<fromApp.AppState>) {}
 
   submitForm(): void {
     for (const i in this.validateForm.controls) {
@@ -28,5 +41,49 @@ export class CheckoutComponent implements OnInit {
       expirationDate: [null, [Validators.required]],
       cvv: [null, [Validators.required]]
     });
+    this.toppingSubscription = this.store.select('toppingReducer').subscribe(toppings => {
+      const totalSelectedCheeses: number = toppings.selectedCheeses.length;
+      const totalSelectedMeats: number = toppings.selectedMeats.length;
+      const totalSelectedVeggies: number = toppings.selectedVeggies.length;
+      const totalMeatCost = totalSelectedMeats * .90;
+      const totalCheeseCost = totalSelectedCheeses * .70;
+      const totalVeggieCost = totalSelectedVeggies * .80;
+      const subtotal = totalMeatCost + totalCheeseCost + totalVeggieCost; //not calculate pizza base price yet
+      const totalTax = null;
+      const totalDue = null;
+
+      this.orderSummary = {
+        totalSelectedMeatTopping: totalSelectedMeats,
+        totalCostForSelectedMeatTopping: totalMeatCost,
+        totalSelectedVeggieTopping: totalSelectedVeggies,
+        totalCostForSelectedVeggieTopping: totalVeggieCost,
+        totalSelectedCheeseTopping: totalSelectedCheeses,
+        totalCostForSelectedCheeseTopping: totalCheeseCost,
+        pizzaBasePrice: null,
+        pizzaSize: null,
+        subtotal: subtotal,
+        taxPercent: '7%',
+        totalTax: null,
+        totalDue: null
+      }
+    });
+
+    this.toppingSubscription = this.store.select('pizzaReducer').subscribe(data => {
+      //pizza size
+      const pizzaSizes: PizzaSize[] = data.pizza.size;
+      const finalSelectedPizzaSize: PizzaSize[] = _.filter(pizzaSizes, {isSelected: true});//guarantee to be 1 at all time
+      const finalPizzaSize = finalSelectedPizzaSize[0].label;
+      this.orderSummary.pizzaSize = finalPizzaSize;
+      this.orderSummary.pizzaBasePrice = data.pizza.price;
+      const newSubTotal = this.orderSummary.subtotal + data.pizza.price;
+      this.orderSummary.subtotal = newSubTotal;
+      const newTotalTax = newSubTotal * .07;
+      this.orderSummary.totalTax = newTotalTax;
+      this.orderSummary.totalDue = newTotalTax + newSubTotal;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.toppingSubscription.unsubscribe();
   }
 }
